4374e2201e68ec4bfa7a4f1c3019f1c294617381
velodo <email@fake.fake>
2022-02-27 10:42:34 -0600

    Updated script and config

---
+++ youtube-dl-script.ps1
@@ -0,0 +1,33 @@
+##################################
+##		YOUTUBE-DL SCRIPT		##
+##################################
+
+
+## Change Directory ##
+
+cd Z:/
+
+
+## Update ##
+
+./yt-dlp --update
+
+
+## Channel Download ##
+
+#Playlists
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_4k_Playlist.conf" -a "zzz_ytdl_settings/ChannelList_4k_Playlist.txt"
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_1080_Playlist.conf" -a "zzz_ytdl_settings/ChannelList_1080_Playlist.txt"
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_720_Playlist.conf" -a "zzz_ytdl_settings/ChannelList_720_Playlist.txt"
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_Audio_Playlist.conf" -a "zzz_ytdl_settings/ChannelList_Audio_Playlist.txt"
+#No Playlists
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_4k_NoPlaylist.conf" -a "zzz_ytdl_settings/ChannelList_4k_NoPlaylist.txt"
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_1080_NoPlaylist.conf" -a "zzz_ytdl_settings/ChannelList_1080_NoPlaylist.txt"
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_720_NoPlaylist.conf" -a "zzz_ytdl_settings/ChannelList_720_NoPlaylist.txt"
+./yt-dlp --config-location "zzz_ytdl_settings/Args_Channel_Audio_NoPlaylist.conf" -a "zzz_ytdl_settings/ChannelList_Audio_NoPlaylist.txt"
+
+
+## Liked Videos ##
+
+#---UNCOMMENT AFTER YOU ADD LINK BELOW!!---
+#youtube-dl --config-location "zzz_ytdl_settings/Args_LikedVideos.conf" [*LINK TO YOUR CHANNELS LIKED VIDEOS PLAYLIST HERE (make sure it's not set to private)*]
